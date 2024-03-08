78cf000b344eafeef1b9ff2128f27178087f1562
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-08 15:59:50 -0300

    Update ContaPoupanca.java
    -Criação do atributo e método visualizar;

--- Generation_Turma_Java_73/ContaBancaria/src/main/java/br/com/generation/model/ContaPoupanca.java
+++ Generation_Turma_Java_73/ContaBancaria/src/main/java/br/com/generation/model/ContaPoupanca.java
@@ -1,5 +1,26 @@
 package br.com.generation.model;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta{
+	
+	private int aniversario;
+	
+	public ContaPoupanca(int numero, int agencia, int tipo, String titular, float saldo, int aniversario) {
+		super(numero, agencia, tipo, titular, saldo);
+		this.aniversario = aniversario;
+	}
+	
+	public void visualizar() {
+		
+	}
+
+	public int getAniversario() {
+		return aniversario;
+	}
+
+	public void setAniversario(int aniversario) {
+		this.aniversario = aniversario;
+	}
+	
+	
 
 }
