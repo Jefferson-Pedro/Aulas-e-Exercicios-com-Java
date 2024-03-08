80f5a692d5df450dfcae118fd2f2a015eeb557b3
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-08 16:45:48 -0300

    Create ContaController.java

---
+++ Generation_Turma_Java_73/ContaBancaria/src/main/java/br/com/generation/controller/ContaController.java
@@ -0,0 +1,46 @@
+package br.com.generation.controller;
+
+import br.com.generation.model.Conta;
+
+public class ContaController {
+	
+
+	public void procurarPorNumero(int numero) {
+		
+	}
+
+	public void listarTodas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void cadastrar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void atualizar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deletar(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void sacar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void depositar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+}
