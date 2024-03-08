c5275b95768e4d7a1c68c92f982e1a059e5c1831
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-08 15:57:51 -0300

    Update ContaBancariaApplication.java

--- Generation_Turma_Java_73/ContaBancaria/src/main/java/br/com/generation/main/ContaBancariaApplication.java
+++ Generation_Turma_Java_73/ContaBancaria/src/main/java/br/com/generation/main/ContaBancariaApplication.java
@@ -1,4 +1,4 @@
-package br.com.generation.ContaBancaria;
+package br.com.generation.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
