900e27a4450f64e3ae10c2e1f467ef769383635f
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-08 16:45:42 -0300

    Create ContaServiceImp.java

---
+++ Generation_Turma_Java_73/ContaBancaria/src/main/java/br/com/generation/service/ContaServiceImp.java
@@ -0,0 +1,55 @@
+package br.com.generation.service;
+
+import br.com.generation.model.Conta;
+
+public class ContaServiceImp implements IContaService{
+
+	@Override
+	public void procurarPorNumero(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarTodas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cadastrar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sacar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depositar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
