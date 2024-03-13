17b4c096714a16f871f3ebc2e521df4e4bddabba
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-13 10:23:54 -0300

    Update ContaRepository.java
    - Reestruturação da interface ContaRepository

--- Generation_Turma_Java_73/ContaBancaria/src/main/java/br/com/generation/repository/ContaRepository.java
+++ Generation_Turma_Java_73/ContaBancaria/src/main/java/br/com/generation/repository/ContaRepository.java
@@ -1,9 +1,19 @@
 package br.com.generation.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import br.com.generation.model.Conta;
 
-public interface ContaRepository extends JpaRepository<Integer, Conta>{
+public interface ContaRepository {
+
+    // CRUD da Conta
+    public abstract void procurarPorNumero(int numero);
+    public void listarTodas();
+    public void cadastrar();
+    public void atualizar();
+    public void deletar(int numero);
+
+    // Métodos Bancários
+    public void sacar(int numero, float valor);
+    public void depositar(int numero, float valor);
+    public void transferir(int numeroOrigem, int numeroDestino, float valor);
 
 }
