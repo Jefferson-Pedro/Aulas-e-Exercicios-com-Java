a2db8b4f3df831f0201ea001e37e22cb22db1bc0
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-16 16:16:08 -0300

    Update ContaRepository.java
    - Alteração na estrutura dos metodos do repository

--- Generation_Turma_Java_73/ContaBancaria/src/main/java/br/com/generation/repository/ContaRepository.java
+++ Generation_Turma_Java_73/ContaBancaria/src/main/java/br/com/generation/repository/ContaRepository.java
@@ -5,15 +5,15 @@ import br.com.generation.model.Conta;
 public interface ContaRepository {
 
     // CRUD da Conta
-    public abstract void procurarPorNumero(int numero);
+    public abstract void procurarPorNumero();
     public void listarTodas();
     public void cadastrar();
     public void atualizar();
-    public void deletar(int numero);
+    public void deletar();
 
     // Métodos Bancários
-    public void sacar(int numero, float valor);
-    public void depositar(int numero, float valor);
-    public void transferir(int numeroOrigem, int numeroDestino, float valor);
+    public void sacar();
+    public void depositar();
+    public void transferir();
 
 }
